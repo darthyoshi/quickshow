4943a38ad2c48c4d02940099db9e7a6d5b36e087
mosesjlee <mosesjlee@gmail.com>
2014-11-10 11:43:37 -0800

    first push

---
+++ src/quickshow/Quickshow.java
@@ -0,0 +1,15 @@
+package quickshow;
+
+import processing.core.PApplet;
+import controlP5.*;
+import ddf.*;
+
+public class Quickshow extends PApplet {
+
+	public void setup() {
+		setSize(900, 600);
+	}
+
+	public void draw() {
+	}
+}
