740684aa6de1944abab28ca070fbab270fa1753e
mosesjlee <mosesjlee@gmail.com>
2014-11-10 14:49:14 -0800

    added classes

--- src/quickshow/Quickshow.java
+++ src/quickshow/Quickshow.java
@@ -4,12 +4,17 @@ import processing.core.PApplet;
 import controlP5.*;
 import ddf.*;
 
+
 public class Quickshow extends PApplet {
 
 	public void setup() {
 		setSize(900, 600);
+		
 	}
 
 	public void draw() {
+	}
+	
+	public void update(){
 	}
 }
---
+++ src/quickshow/audiolistUI.java
@@ -0,0 +1,5 @@
+package quickshow;
+
+public class audiolistUI {
+
+}
