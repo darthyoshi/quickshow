520f7e80b8aae0d1accde99bf6dd015b50bc549d
mosesjlee <mosesjlee@gmail.com>
2014-11-16 16:04:02 -0800

    Added slideshow class

--- src/quickshow/slideShow.java
+++ src/quickshow/slideShow.java
@@ -1,11 +1,43 @@
 package quickshow;
 
+import java.util.ArrayList;
+import processing.video.*;
+import quickshow.datatypes.MediaItem;
+
 public class slideShow {
-	//MovieMaker show;
+	MovieMaker show;
+	ArrayList <MediaItem> items;
 	
-	public slideShow(){
+	public slideShow(ArrayList<MediaItem> listOfItems){
 		//Constructor
 	}
+	
+	public void parseMediaItems(){
+		
+	}
+	
+	public void generateShow(){
+		
+	}
+	
+	
+	//For playback functionality
+	public void pause(){
+		
+	}
+	
+	public void play(){
+		
+	}
+	
+	public void stop(){
+		
+	}
+	
+	/*
+	 * Add helper functions below here
+	 */
+	
 	
 	
 }
