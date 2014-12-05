533a7c366d2d3520db43214a78b0f561c76ba8b4
Kay Choi <kay.n.choi@gmail.com>
2014-12-05 00:59:16 -0800

    Whitespace trimming, and tag time getter methods now return absolute values.

--- src/quickshow/PopupDialogue.java
+++ src/quickshow/PopupDialogue.java
@@ -2,7 +2,7 @@
  * @file PopupDialogue.java
  * @author Kay Choi
  * @description A popup dialogue for modifying VisualItem parameters in the
- *   slide show. 
+ *   slide show.
  */
 
 package quickshow;
@@ -21,12 +21,12 @@ import controlP5.Textfield;
 
 public class PopupDialogue {
     private boolean debug;
-    
+
     private ControlP5 controlp5;
     private Quickshow parent;
-    
+
     private VisualItem item = null;
-    
+
     private Group popupGroup;
     private static final int[] popupOrigin = {350, 200};
     private Slider imgDisplaySlider;
@@ -43,27 +43,27 @@ public class PopupDialogue {
     public PopupDialogue(Quickshow parent, ControlP5 control) {
         this.parent = parent;
         this.controlp5 = control;
-        
+
         debug = parent.getDebugFlag();
 
         popupGroup = controlp5.addGroup("popupUI")
             .setLabel("")
             .setVisible(false);
-        
+
         popupLock = new Controller[6];
-        
+
         controlp5.addTextarea("tagFieldLabel")
             .setPosition(popupOrigin[0], popupOrigin[1]+20)
             .setText("SET CAPTION TEXT")
             .setGroup(popupGroup);
-        
+
         controlp5.addTextarea("tagLabel")
             .setPosition(popupOrigin[0], popupOrigin[1]+42)
             .setText("CAPTION TIME")
             .setGroup(popupGroup);
-        
+
         int[] lblOffset = new int[2];
-        
+
         lblOffset[0] = 130;
         lblOffset[1] = 40;
         controlp5.addTextarea("tagStartLabel")
@@ -79,7 +79,7 @@ public class PopupDialogue {
             .setAutoClear(false)
             .lock()
             .setGroup(popupGroup);
-        
+
         lblOffset[0] = 210;
         controlp5.addTextarea("tagEndLabel")
             .setPosition(popupOrigin[0] + lblOffset[0] - 28,
@@ -94,7 +94,7 @@ public class PopupDialogue {
             .setAutoClear(false)
             .lock()
             .setGroup(popupGroup);
-        
+
         popupLock[2] = tagField = controlp5.addTextfield("tagText")
             .setPosition(popupOrigin[0], popupOrigin[1])
             .setSize(250, 20)
@@ -102,7 +102,7 @@ public class PopupDialogue {
             .setAutoClear(false)
             .lock()
             .setGroup(popupGroup);
-        
+
         popupLock[3] = imgDisplaySlider = controlp5.addSlider("Image Display Time")
             .setNumberOfTickMarks(9)
             .setSize(250, 10)
@@ -113,14 +113,14 @@ public class PopupDialogue {
             .setGroup(popupGroup);
         imgDisplaySlider.getCaptionLabel()
             .align(ControlP5.LEFT, ControlP5.BOTTOM_OUTSIDE).setPaddingX(0);
-        
+
         popupLock[4] = popupAccept = controlp5.addButton("Accept")
             .lock()
             .setSize(70, 15)
             .setPosition(popupOrigin[0] + 105, popupOrigin[1] + 105)
             .setGroup(popupGroup);
         popupAccept.getCaptionLabel().alignX(ControlP5Constants.CENTER);
-        
+
         popupLock[5] = popupCancel = controlp5.addButton("Cancel")
             .lock()
             .setSize(70, 15)
@@ -137,29 +137,29 @@ public class PopupDialogue {
      */
     public void togglePopup(boolean toggle, VisualItem item, Integer offset) {
         this.item = item;
-        
+
         if(!toggle || item != null) {
-        	popupGroup.setVisible(toggle);
-	        
-	        for(Controller popup : popupLock) {
-	            popup.setLock(!toggle);
-	        }
+            popupGroup.setVisible(toggle);
+
+            for(Controller popup : popupLock) {
+                popup.setLock(!toggle);
+            }
         }
-        
+
         if(item != null) {
             if(item.checkType().equalsIgnoreCase("video")) {
                 imgDisplaySlider.setVisible(false);
                 imgDisplaySlider.lock();
             }
-            
+
             else {
                 imgDisplaySlider.setValue(item.getDisplayTime());
             }
-                
+
             this.offset = offset;
         }
     }
-    
+
     /**
      * Checks the state of the popup dialogue.
      * @return true if popup dialogue is open
@@ -167,7 +167,7 @@ public class PopupDialogue {
     public boolean isEnabled() {
         return popupGroup.isVisible();
     }
-    
+
     /**
      * Retrieves the Cartesian coordinates of the top left corner of the popup
      *   dialogue.
@@ -183,9 +183,9 @@ public class PopupDialogue {
      */
     private String getTagText() {
         String result = tagField.getText();
-        
+
         tagField.setText("");
-        
+
         return result;
     }
 
@@ -195,37 +195,37 @@ public class PopupDialogue {
      */
     private int getTagStartTime() {
         int result;
-        
+
         try {
-            result = Integer.parseInt(tagStartField.getText());
+            result = Math.abs(Integer.parseInt(tagStartField.getText()));
         }
 
         catch(NumberFormatException e) {
             result = 0;
         }
-        
+
         tagStartField.setText("");
-        
+
         return result;
     }
-    
+
     /**
      * Retrieves the value of the end time Textfield.
      * @return integer
      */
     private int getTagEndTime() {
         int result;
-        
+
         try {
-            result = Integer.parseInt(tagEndField.getText());
+            result = Math.abs(Integer.parseInt(tagEndField.getText()));
         }
-        
+
         catch(NumberFormatException e) {
             result = 0;
         }
-        
+
         tagEndField.setText("");
-        
+
         return result;
     }
 
@@ -237,7 +237,7 @@ public class PopupDialogue {
         parent.fill(0xff3333aa);
         parent.rect(popupOrigin[0]-10, popupOrigin[1]-10, 270, 140);
     }
-    
+
     /**
      * Callback method for handling ControlP5 UI events.
      * @param event the ControlEvent to handle
@@ -248,40 +248,40 @@ public class PopupDialogue {
             String text = getTagText();
             int startTime = -1;
             int endTime = -1;
-            
+
             if(debug) {
                 Quickshow.println("Caption text: " + text);
             }
-            
+
             if(!text.trim().equals("")) {
                 startTime = getTagStartTime();
                 endTime = getTagEndTime();
                 item.setTag(0, text, startTime - offset, endTime - offset);
             }
-            
+
             int duration = (int)imgDisplaySlider.getValue();
-                
+
             if(debug) {
                 Quickshow.println(
-                    "Caption start: " + (startTime >= 0 ? startTime : "N/A") + 
+                    "Caption start: " + (startTime >= 0 ? startTime : "N/A") +
                     "s\nCaption end: " + (endTime >= 0 ? endTime : "N/A") +
                     "s\nImage duration: " + duration
                 );
             }
-              
+
             if(item.checkType().equalsIgnoreCase("image")) {
                 ((ImageItem)item).setDisplayTime(duration);
             }
-        
+
             togglePopup(false, null, null);
-            
+
             break;
-            
+
         case "Cancel":
             togglePopup(false, null, null);
-            
+
             break;
         }
-        
+
     }
 }
