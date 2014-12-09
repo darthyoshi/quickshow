32551015f1a7f6d5d88a9fbe84c49d04329190fc
Kay Choi <kay.n.choi@gmail.com>
2014-12-08 18:41:26 -0800

    Changed color of selection markers.

--- src/quickshow/visualthumbnailUI.java
+++ src/quickshow/visualthumbnailUI.java
@@ -93,13 +93,13 @@ public class visualthumbnailUI {
 
                     //selected highlight
                     if(selectedIndex.hasValue(j)) {
-                        parent.stroke(0xff5522ff);
+                        parent.stroke(0xff22aaff);
                         parent.noFill();
                         parent.rectMode(PConstants.CENTER);
                         parent.rect(xStartIndex, yStartIndex,
                             MAX_THUMBNAIL_WIDTH - 2, MAX_THUMBNAIL_HEIGHT - 4);
 
-                        parent.fill(0);
+                        parent.fill(0xff22aaff);
                         parent.text(
                             selectedIndex.index(j)+1,
                             10 + xStartIndex - MAX_THUMBNAIL_WIDTH/2,
